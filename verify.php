e6c834a653d6f833b686c4b0e5947962e86fd41d
icedzax <ice_dogzax@hotmail.com>
2018-09-15 10:42:27 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'ZijJtDMIslEM2w2ZhCyOrcSMm9chVRZAHag8Jx5XsoaOog4qjjq+qP0uDKmaD349T1UHczkQJWYSwLyzMEj0bSHC1Fu6ZdbnW5638pK31QYxgiUl1myc20OSyEFf5lSnbF9HcRsPS8APHIH3EsM1LgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'No9UgUHojZ5MXMEcdTL/2aPRER3MyxFCdlsgCf5AIYTyNSEQycop/fJ+eBOP4iLzT1UHczkQJWYSwLyzMEj0bSHC1Fu6ZdbnW5638pK31Qbi9vnBX4cBf8n1ALpZuSZB/8Ql3/x6bLO0f9KKh40WJwdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
